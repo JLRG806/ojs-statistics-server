3258d3fcdc7f8c56bcd37b8dea43b510cf0250fc
--add <jlrg.dev@gmail.com>
2024-06-12 12:55:59 +0200

    feature: added status code validations.

--- src/server.js
+++ src/server.js
@@ -12,7 +12,7 @@ server.use(morgan('dev'))
 
 server.get('/stats/issues/timeline', async (req, res) => {
 
-    const { dateStart, dateEnd, timelineInterval, type, context } = req.query
+    const { dateStart, dateEnd, timelineInterval, context, type } = req.query
 
     const isTimelineIntervalInvalid = timelineIntervalSchemaHandler(timelineInterval)
 
@@ -20,7 +20,7 @@ server.get('/stats/issues/timeline', async (req, res) => {
         return res.status(400).json({ error: isTimelineIntervalInvalid.error });
     }
 
-    const isStatsIssuesTimelineSchemaInvalid = statsIssuesTimelineSchemaHandler(dateStart, dateEnd, timelineInterval, type)
+    const isStatsIssuesTimelineSchemaInvalid = statsIssuesTimelineSchemaHandler(dateStart, dateEnd, timelineInterval, context, type)
     if (isStatsIssuesTimelineSchemaInvalid) {
         return res.status(400).json({ error: isStatsIssuesTimelineSchemaInvalid.error });
     }
@@ -33,23 +33,37 @@ server.get('/stats/issues/timeline', async (req, res) => {
 
     setGlobalDispatcher(agent)
 
-    const response = await fetch(config.ojs.OJS_DOMAIN + '/index.php/quimera/api/v1/stats/issues/timeline?' + new URLSearchParams({
-        dateStart: dateStart,
-        dateEnd: dateEnd,
-        timelineInterval: timelineInterval,
-        type: type
-    }), {
-        method: 'GET',
-        headers: {
-            'Authorization': 'Bearer ' + config.ojs.API_KEY,
-            'Content-Type': 'application/json',
-            //'User-Agent': 'PostmanRuntime/7.37.3',
-            'Accept': '*/*',
-            'Accept-Encoding': 'gzip, deflate, br'
+    try {
+        const response = await fetch(config.ojs.OJS_DOMAIN + '/index.php/' + context + '/api/v1/stats/issues/timeline?' + new URLSearchParams({
+            dateStart: dateStart,
+            dateEnd: dateEnd,
+            timelineInterval: timelineInterval,
+            type: type
+        }), {
+            method: 'GET',
+            headers: {
+                'Authorization': 'Bearer ' + config.ojs.API_KEY,
+                'Content-Type': 'application/json',
+                //'User-Agent': 'PostmanRuntime/7.37.3',
+                'Accept': '*/*',
+                'Accept-Encoding': 'gzip, deflate, br'
+            }
+        })
+
+        console.log(response)
+        const statusCode = response.status
+        if (statusCode >= 400 && statusCode <= 499) {
+            return res.sendStatus(statusCode)
         }
-    })
-    console.log(response)
-    res.json(await response.json())
+
+        if (statusCode >= 500 && statusCode <= 599) {
+            return res.sendStatus(statusCode)
+        }
+        return res.json(await response.json())
+    } catch (error) {
+        console.error(error)
+        return res.status(500).json({ error: error })
+    }
 })
 
 // Server
